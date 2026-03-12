c977bff0b6490cfe2a7bdc2133e866f1242f5ded
salomeriverod-hue <salomeriverod@gmail.com>
2026-03-12 13:38:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ with st.expander('Analizar texto'):
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
            with open('Cat.json') as source:
-        data = source.read()
+             animation=json.load(source)
+         st.lottie(animation,width =350)
+   
   
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
