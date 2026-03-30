fa9f21b3661ad3d33be8d91fa8a86ec9d0927f38
salomeriverod-hue <salomeriverod@gmail.com>
2026-03-29 21:15:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,12 @@ with st.expander('Analizar texto'):
   
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
+            with open('Emoji - Sad.json') as source:
+             animation=json.load(source)
+            st.lottie(animation,width =350)
+          
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+            with open('Meditation.json') as source:
+             animation=json.load(source)
+            st.lottie(animation,width =350)
